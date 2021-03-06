ae8576944b65a5494db39ef8a37ffc0044475ca2
Jedro1270 <jedro1270@gmail.com>
2021-03-06 09:04:16 +0800

    refactor: refactor text form field

--- lib/components/login.dart
+++ lib/components/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wellness24/components/common/text_input.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -27,52 +28,25 @@ class _LoginState extends State<Login> {
                   ),
                 ),
                 SizedBox(height: 50.0),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey.withOpacity(0.5),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 25,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12)),
-                  ),
+                TextInput(
+                  hint: 'Email',
                 ),
                 SizedBox(height: 30.0),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey.withOpacity(0.5),
-                    hintText: 'Password',
-                    hintStyle: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 25,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12)),
-                  ),
-                ),
-
-                 SizedBox(height: 20.0),
+                TextInput(hint: 'Password'),
+                SizedBox(height: 20.0),
                 MaterialButton(
-                  shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  side: BorderSide(color: Colors.black12)),
-                  color: Colors.grey.withOpacity(0.5),
-                  minWidth: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.fromLTRB(18.0, 15.0, 18.0, 15.0),
-
-                  onPressed: () {},
-                  child: Text("Login",
-                  style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 25,))
-                ),
-
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        side: BorderSide(color: Colors.black12)),
+                    color: Colors.grey.withOpacity(0.5),
+                    minWidth: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.fromLTRB(18.0, 15.0, 18.0, 15.0),
+                    onPressed: () {},
+                    child: Text("Login",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 25,
+                        ))),
                 SizedBox(height: 20.0),
                 Container(
                   child: Row(
