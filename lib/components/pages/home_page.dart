28582c05a7fa579e192342be73836754a4cb2344
elim23 <elimjoyce.abagat-19@cpu.edu.ph>
2021-03-09 15:31:13 +0800

    feat: add home page

--- lib/components/pages/home_page.dart
+++ lib/components/pages/home_page.dart
@@ -10,13 +10,122 @@ class _HomeState extends State<Home> {
   @override
 
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
+          centerTitle: true,
+          toolbarHeight: 75,
           title: Text('Wellness24'),
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(icon: Icon(Icons.notifications), 
+            onPressed: (){
+              print("Clicked Notif icon");
+            }
+            )
+          ],
         ),
-        //icon: new Icon(Icons.home_filled)
-      ),
+
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.message),
+          onPressed: (){
+            print("Message Icon click");
+          },),
+        
+        body: Column(
+          children: [
+            Row(
+              
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Search Doctor',
+                        suffixIcon: Icon(Icons.search),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide(color: Colors.black)
+                        )
+                        ),
+                    )
+                  )
+                ),
+                Container(
+                  child: Icon(Icons.filter_list))
+              ],
+                
+              ), 
+              Container(
+                child: Row(children: <Widget>[
+                  Text("EMERGENCY", 
+                  style: TextStyle(
+                    fontSize: 20, 
+                    fontWeight:  FontWeight.bold,
+                    color: Colors.white)),
+                  
+                ],
+                ),
+                color: Colors.redAccent[700],
+                height: 75
+              ),
+              
+              Divider(height: 50),
+
+              Container(
+                height: 75,
+                padding: EdgeInsets.all(25),
+                color: Colors.lightBlueAccent[100],
+                child: Row(children: <Widget>[
+                  Text("Doctor's Info", 
+                  style: TextStyle(
+                    fontSize: 20, 
+                    fontWeight:  FontWeight.bold,
+                    color: Colors.black)
+                  )
+                ],
+                )
+              ),
+
+              Divider(height: 1, thickness: 2, color: Colors.lightBlueAccent[200]),
+              
+              Container(
+                height: 75,
+                padding: EdgeInsets.all(25),
+                color: Colors.lightBlueAccent[100],
+                child: Row(
+                  children: <Widget>[
+                  Text("Schedule Appointment", 
+                  style: TextStyle(
+                    fontSize: 20, 
+                    fontWeight:  FontWeight.bold,
+                    color: Colors.black)
+                  ),
+                  
+                ],
+                )
+              ),
+
+              Divider(height: 1, thickness: 2, color: Colors.lightBlueAccent[200]),
+              
+              Container(
+                height: 75,
+                padding: EdgeInsets.all(25),
+                color: Colors.lightBlueAccent[100],
+                child: Row(children: <Widget>[
+                  Text("My Medical Records", 
+                  style: TextStyle(
+                    fontSize: 20, 
+                    fontWeight:  FontWeight.bold,
+                    color: Colors.black)
+                  )
+                ],
+                )
+              ),
+
+            ],
+
+        )
     );
   }
 }
