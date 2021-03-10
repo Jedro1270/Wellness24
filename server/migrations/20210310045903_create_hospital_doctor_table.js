68b4857e3a0b633ddd39282b56b448443b85e456
Veto Bastiero <vetobastiero@gmail.com>
2021-03-10 13:56:24 +0800

    fix: adds schema to table

--- server/migrations/20210310045903_create_hospital_doctor_table.js
+++ server/migrations/20210310045903_create_hospital_doctor_table.js
@@ -1,13 +1,13 @@
 exports.up = function (knex) {
   return knex.raw(`
-    CREATE TABLE hospital_doctor (
-      hospital_id integer REFERENCES hospital(hospital_id) NOT NULL,
-      doctor_id integer REFERENCES doctor(doctor_id) NOT NULL,
+    CREATE TABLE app.hospital_doctor (
+      hospital_id integer REFERENCES app.hospital(hospital_id) NOT NULL,
+      doctor_id integer REFERENCES app.doctor(doctor_id) NOT NULL,
       PRIMARY KEY (hospital_id, doctor_id)
     );
 
-    CREATE INDEX hospital_doctor_hospital_id_fkey ON hospital_doctor(hospital_id);
-    CREATE INDEX hospital_doctor_doctor_id_fkey ON hospital_doctor(doctor_id);
+    CREATE INDEX hospital_doctor_hospital_id_fkey ON app.hospital_doctor(hospital_id);
+    CREATE INDEX hospital_doctor_doctor_id_fkey ON app.hospital_doctor(doctor_id);
   `);
 };
 
@@ -16,6 +16,6 @@ exports.down = function (knex) {
     DROP INDEX hospital_doctor_doctor_id_fkey;
     DROP INDEX hospital_doctor_hospital_id_fkey;
     
-    DROP TABLE hospital_doctor;  
+    DROP TABLE app.hospital_doctor;  
   `);
 };
