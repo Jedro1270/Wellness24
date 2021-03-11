9dfb4d1d60fc781b504717c2b46952519f74b3f7
Veto Bastiero <vetobastiero@gmail.com>
2021-03-11 17:48:36 +0800

    feat: adds auth wrapper

---
+++ lib/components/auth/auth_wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:wellness24/components/pages/front_page.dart';
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //return login or front page
+    return FrontPage();
+  }
+}
