d1eca4c1d7328e3ccb10b99b128fae9d09e78c97
Veto Bastiero <vetobastiero@gmail.com>
2021-03-11 19:07:47 +0800

    feat: returns custom user model instead of firebase user obj

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,19 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:wellness24/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFirebase(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
       AuthResult result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebase(user);
     } catch (e) {
       print(e.toString());
       return null;
     }
   }
-
-  
 }
