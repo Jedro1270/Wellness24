e8f2e787db61de21b3efcf84dea6c7e62ce418d8
Veto Bastiero <vetobastiero@gmail.com>
2021-03-11 22:19:38 +0800

    feat: conditionally renders frontpage based on auth status

--- lib/components/auth/auth_wrapper.dart
+++ lib/components/auth/auth_wrapper.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wellness24/components/pages/front_page.dart';
+import 'package:wellness24/components/pages/home_page.dart';
+import 'package:wellness24/models/user.dart';
 
 class AuthWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
 
     //return login or front page
-    return FrontPage();
+    return user == null ? FrontPage() : Home();
   }
 }
