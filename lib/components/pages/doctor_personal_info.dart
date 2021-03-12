88450e331f1bc6e65ac2b09d7feb5f9850de520f
Veto Bastiero <vetobastiero@gmail.com>
2021-03-12 17:52:05 +0800

    feat: adds create account logic on personal info

--- lib/components/pages/doctor_personal_info.dart
+++ lib/components/pages/doctor_personal_info.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:wellness24/components/common/text_input.dart';
 import 'package:wellness24/components/pages/doctor_profession_info.dart';
+import 'package:wellness24/models/new_account.dart';
 
 class DoctorPersonalInfo extends StatefulWidget {
+  final NewAccount account;
+  DoctorPersonalInfo(this.account);
+
   @override
-  _DoctorPersonalInfoState createState() => _DoctorPersonalInfoState();
+  _DoctorPersonalInfoState createState() =>
+      _DoctorPersonalInfoState(this.account);
 }
 
 class _DoctorPersonalInfoState extends State<DoctorPersonalInfo> {
-  dynamic selectedRadio;
+  final _formKey = GlobalKey<FormState>();
+  String selectedRadio, lastName, firstName, middleInitial, birthDate, address;
+  NewAccount account;
+
+  _DoctorPersonalInfoState(this.account);
 
   @override
   void initState() {
     super.initState();
-    selectedRadio = 0;
+    selectedRadio = '';
   }
 
-  setSelectedRadio(int val) {
-    setState(() {
-      selectedRadio = val;
-    });
-  }
+  changeState(String variable) => (String val) => {
+        setState(() {
+          variable = val;
+        })
+      };
 
   @override
   Widget build(BuildContext context) {
@@ -28,152 +37,191 @@ class _DoctorPersonalInfoState extends State<DoctorPersonalInfo> {
       backgroundColor: Colors.white,
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 25.0),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              // padding: EdgeInsets.symmetric(horizontal: 80.0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "Personal Information Form",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              child: Row(children: <Widget>[
-                Text(
-                  "Last Name",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
-                ),
-              ]),
-            ),
-            SizedBox(height: 10),
-            SizedBox(height: 50, child: TextInput(obscureText: false)),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text(
-                  "First Name",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
-                ),
-              ]),
-            ),
-            SizedBox(height: 10),
-            SizedBox(height: 50, child: TextInput(obscureText: false)),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text(
-                  "Middile Name",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
-                ),
-              ]),
-            ),
-            SizedBox(height: 10),
-            SizedBox(height: 50, child: TextInput(obscureText: false)),
-            Text(
-              "Gender",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: "ShipporiMincho",
-                  fontWeight: FontWeight.normal),
-            ),
-            Container(
-              child: Row(children: <Widget>[
-                Radio(
-                  value: 1,
-                  groupValue: selectedRadio,
-                  activeColor: Colors.blueAccent,
-                  onChanged: (val) {
-                    print("Radio $val");
-                    setSelectedRadio(val);
-                  },
-                ),
-                Text(
-                  "Male",
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
-                ),
-                Radio(
-                  value: 2,
-                  groupValue: selectedRadio,
-                  activeColor: Colors.blueAccent,
-                  onChanged: (val) {
-                    print("Radio $val");
-                    setSelectedRadio(val);
-                  },
-                ),
-                Text(
-                  "Female",
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
-                )
-              ]),
-            ),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text("Birth Date",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.normal)),
-              ]),
-            ),
-            SizedBox(height: 5),
-            SizedBox(height: 50, child: TextInput(hint: "mm-dd-yy",obscureText: false)),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text("Address",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.normal)),
-              ]),
-            ),
-            SizedBox(height: 5),
-            SizedBox(height: 50, child: TextInput(obscureText: false)),
-            SizedBox(height: 50),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward_sharp, size: 45),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DoctorProfessionInfo()),
-                      );
-                    },
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  // padding: EdgeInsets.symmetric(horizontal: 80.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Personal Information Form",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text(
+                      "Last Name",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                        obscureText: false,
+                        onChanged: (val) => setState(() => lastName = val),
+                        validator: (val) =>
+                            val.isEmpty ? 'This field is required' : null)),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text(
+                      "First Name",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                        obscureText: false,
+                        onChanged: (val) => setState(() => firstName = val),
+                        validator: (val) =>
+                            val.isEmpty ? 'This field is required' : null)),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text(
+                      "Middle Initial",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                        obscureText: false,
+                        onChanged: (val) => setState(() => middleInitial = val),
+                        validator: (val) => val.isEmpty
+                            ? 'This field is required'
+                            : val.length > 1
+                                ? 'Please enter 1 character only'
+                                : null)),
+                Text(
+                  "Gender",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: "ShipporiMincho",
+                      fontWeight: FontWeight.normal),
+                ),
+                Container(
+                  child: Row(children: <Widget>[
+                    Radio(
+                      value: 'Male',
+                      groupValue: selectedRadio,
+                      activeColor: Colors.blueAccent,
+                      onChanged: (val) => setState(() => selectedRadio = val),
+                    ),
+                    Text(
+                      "Male",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                    Radio(
+                        value: 'Female',
+                        groupValue: selectedRadio,
+                        activeColor: Colors.blueAccent,
+                        onChanged: (val) =>
+                            setState(() => selectedRadio = val)),
+                    Text(
+                      "Female",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    )
+                  ]),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text("Birth Date",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.normal)),
+                  ]),
+                ),
+                SizedBox(height: 5),
+                SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                        decoration: InputDecoration(hintText: 'mm-dd-yy'),
+                        obscureText: false,
+                        onChanged: (val) => setState(() => birthDate = val),
+                        validator: (val) =>
+                            val.isEmpty ? 'This field is required' : null)),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text("Address",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.normal)),
+                  ]),
+                ),
+                SizedBox(height: 5),
+                SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                        obscureText: false,
+                        onChanged: (val) => setState(() => address = val),
+                        validator: (val) =>
+                            val.isEmpty ? 'This field is required' : null)),
+                SizedBox(height: 50),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.arrow_forward_sharp, size: 45),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            account.supplyPersonalInfo(
+                                lastName: lastName,
+                                firstName: firstName,
+                                middleInitial: middleInitial,
+                                gender: selectedRadio,
+                                birthDate: birthDate,
+                                address: address);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DoctorProfessionInfo(account)),
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
