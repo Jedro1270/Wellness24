5a7411aabb327671a274b0b12d630d0bba0f5aee
Veto Bastiero <vetobastiero@gmail.com>
2021-03-12 16:09:18 +0800

    feat: adds new account class

---
+++ lib/models/new_account.dart
@@ -0,0 +1,31 @@
+import 'package:wellness24/services/auth_service.dart';
+
+class NewAccount {
+  String role;
+  String email;
+  String password;
+  String contactNo;
+  String lastName;
+  String firstName;
+  String middleInitial;
+  String birthDate;
+  String address;
+  String gender;
+
+  NewAccount({this.role, this.email, this.contactNo, this.password});
+
+  void supplyPersonalInfo(
+      {lastName, firstName, middleInitial, gender, birthDate, address}) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.middleInitial = middleInitial;
+    this.gender = gender;
+    this.birthDate = birthDate;
+    this.address = address;
+  }
+
+  void registerDoctor({licenseNo, clinicLocation, clinicStart, clinicEnd}) {
+    final auth = AuthService();
+    auth.registerWithEmailAndPassword(this.email, this.password);
+  }
+}
