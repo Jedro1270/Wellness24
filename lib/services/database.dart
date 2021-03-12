66cb377a036b0a69ef0114aceae0ccd78bd2cede
Jedro1270 <jedro1270@gmail.com>
2021-03-12 21:48:00 +0800

    feat: create insert doctor method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,13 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:wellness24/models/new_account.dart';
 
 class DatabaseService {
   final String uid;
   DatabaseService({this.uid});
 
   final CollectionReference roles = Firestore.instance.collection('roles');
+  final CollectionReference doctors = Firestore.instance.collection('doctors');
 
   //gets user role "doctor/patient"
   Future getRole() async {
     return await roles.document(uid).get();
+  }
+
+  void insertDoctor(NewAccount doctorAccount) {
+    doctors.document(uid).setData({
+      'keywords': doctorAccount.keywords,
+      'email': doctorAccount.email,
+      'password': doctorAccount.password,
+      'contactNumber': doctorAccount.contactNo,
+      'lastName': doctorAccount.lastName,
+      'firstName': doctorAccount.firstName,
+      'middleInitial': doctorAccount.middleInitial,
+      'birthDate': doctorAccount.birthDate,
+      'address': doctorAccount.address,
+      'gender': doctorAccount.gender
+    });
   }
 }
