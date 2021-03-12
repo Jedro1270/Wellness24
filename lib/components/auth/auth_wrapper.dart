96dc7aa907c592be45f1daf686e957238d7c0cc1
Alejandro Jonela III <ajonela46@gmail.com>
2021-03-12 10:06:25 +0800

    chore: change import path for home page

--- lib/components/auth/auth_wrapper.dart
+++ lib/components/auth/auth_wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wellness24/components/pages/front_page.dart';
-import 'package:wellness24/components/pages/home_page.dart';
+import 'package:wellness24/components/pages/patient_home_page.dart';
 import 'package:wellness24/models/user.dart';
 
 class AuthWrapper extends StatelessWidget {
