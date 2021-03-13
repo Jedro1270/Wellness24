fa6d7e0e29fe4a4b7439d8b33a53d2a05bab74da
Jedro1270 <jedro1270@gmail.com>
2021-03-13 08:42:22 +0800

    feat: create functional log out

--- lib/components/common/navigation_bar.dart
+++ lib/components/common/navigation_bar.dart
@@ -1,69 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:wellness24/services/auth_service.dart';
 
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text('Sapphire Tech'),
-            accountEmail: Text('sapphiretech@gmail.com'),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
+        child: ListView(
+      children: [
+        UserAccountsDrawerHeader(
+          accountName: Text('Sapphire Tech'),
+          accountEmail: Text('sapphiretech@gmail.com'),
+          currentAccountPicture: CircleAvatar(
+            child: ClipOval(
                 child: Image(
-                  image: AssetImage('assets/logo.jpg'),
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
-                )
-              ),
-            ),
+              image: AssetImage('assets/logo.jpg'),
+              width: 90,
+              height: 90,
+              fit: BoxFit.cover,
+            )),
           ),
-          ListTile(
-            leading: Icon(
-              Icons.home_outlined,
-              color: Colors.blueAccent,
-            ),
-            title: Text('Home'),
-            onTap: () => print("Home"),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.home_outlined,
+            color: Colors.blueAccent,
           ),
-          ListTile(
-            leading: Icon(
-              Icons.notifications_outlined,
-              color: Colors.blueAccent
-            ),
-            title: Text('Notifications'),
-            onTap: () => print("Notifications"),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.person_outline,
-              color: Colors.blueAccent
-            ),
-            title: Text('My Patients'),
-            onTap: () => print("My Patients"),
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(
-              Icons.settings_outlined,
-              color: Colors.blueAccent
-            ),
-            title: Text('Settings'),
-            onTap: () => print("Settings"),
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(
-              Icons.logout,
-              color: Colors.blueAccent
-            ),
-            title: Text('Log out'),
-            onTap: () => print("Log out"),
-          ),
-        ],
-      )
-    );
+          title: Text('Home'),
+          onTap: () => print("Home"),
+        ),
+        ListTile(
+          leading: Icon(Icons.notifications_outlined, color: Colors.blueAccent),
+          title: Text('Notifications'),
+          onTap: () => print("Notifications"),
+        ),
+        ListTile(
+          leading: Icon(Icons.person_outline, color: Colors.blueAccent),
+          title: Text('My Patients'),
+          onTap: () => print("My Patients"),
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.settings_outlined, color: Colors.blueAccent),
+          title: Text('Settings'),
+          onTap: () => print("Settings"),
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.logout, color: Colors.blueAccent),
+          title: Text('Log out'),
+          onTap: () {
+            print("Log out");
+            AuthService auth = AuthService();
+
+            auth.signOut();
+          },
+        ),
+      ],
+    ));
   }
 }
