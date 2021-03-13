3a02fbdab065e457b3b09a494bb3d64fab22b6bd
Jedro1270 <jedro1270@gmail.com>
2021-03-14 07:49:04 +0800

    chore: remove unused import

--- lib/components/pages/doctor_search_page/doctors_list.dart
+++ lib/components/pages/doctor_search_page/doctors_list.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wellness24/components/pages/doctor_search_page/doctor_info.dart';
 import 'package:wellness24/services/database.dart';
