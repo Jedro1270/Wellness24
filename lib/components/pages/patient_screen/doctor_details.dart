2cc52981b31a61baaa2db2483c9a95cee1cb5513
Jedro1270 <jedro1270@gmail.com>
2021-03-16 18:56:41 +0800

    feat: add doctor details from parameters

---
+++ lib/components/pages/patient_screen/doctor_details.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:wellness24/components/common/app_bar.dart';
+import 'package:wellness24/models/doctor.dart';
+
+class DoctorDetails extends StatelessWidget {
+  final Doctor doctor;
+
+  DoctorDetails({this.doctor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: 'Doctor',
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.home_outlined),
+            iconSize: 30.0,
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/doctor.png"),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width: double.infinity,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25.0),
+                          topRight: Radius.circular(25.0),
+                        ),
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '${doctor.fullname}\n',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontFamily: "ShipporiMincho",
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              TextSpan(
+                                  text: "${doctor.specialization}",
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontFamily: "ShipporiMincho",
+                                      color: Colors.black)),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                      ButtonAction(
+                        color: Color(0xFFFFB755),
+                        icon: Icons.mail,
+                      ),
+                      ButtonAction(
+                        color: Color(0xFF58c697),
+                        icon: Icons.phone,
+                      )
+                    ]),
+                  ),
+                  Divider(color: Color(0xFFA9A8A8)),
+                  Text("About",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black)),
+                  SizedBox(height: 20.0),
+                  Text("${doctor.about}",
+                      style: TextStyle(
+                          fontSize: 14.0, fontFamily: "ShipporiMincho")),
+                  SizedBox(height: 20.0),
+                  Text("Working hours",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black)),
+                  SizedBox(height: 10.0),
+                  Text(
+                      "${doctor.workingDays}: ${doctor.clinicStartHour} - ${doctor.clinicEndHour}",
+                      style: TextStyle(
+                          fontSize: 14.0, fontFamily: "ShipporiMincho")),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: Center(
+                      child: SizedBox(
+                        width: 90.0,
+                        height: 35.0,
+                        child: RaisedButton(
+                          onPressed: () {},
+                          color: Color(0xFFDBF3E8),
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7.0),
+                          ),
+                          child: Text(
+                            "Open",
+                            style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontFamily: "ShipporiMincho",
+                                    color: Colors.white)
+                                .copyWith(color: Color(0xFF58c697)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 55.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  elevation: 0.0,
+                  color: Color(0xFF40BEEE),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Make an appointment",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "ShipporiMincho",
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonAction extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  ButtonAction({this.color, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      padding: EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14.0),
+        color: color,
+      ),
+      child: Icon(
+        icon,
+        size: 25.0,
+        color: Colors.white,
+      ),
+    );
+  }
+}
