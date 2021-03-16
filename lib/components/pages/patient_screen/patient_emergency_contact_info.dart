d9bb6d189dc83e833427b3623e5930d2ab801a5f
Veto Bastiero <vetobastiero@gmail.com>
2021-03-16 14:22:39 +0800

    feat: initializes inputs for emerg contact

--- lib/components/pages/patient_screen/patient_emergency_contact_info.dart
+++ lib/components/pages/patient_screen/patient_emergency_contact_info.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wellness24/components/common/text_input.dart';
 import 'package:wellness24/components/pages/patient_screen/medical_history.dart';
 import 'package:wellness24/models/new_account.dart';
 
@@ -13,129 +12,177 @@ class EmergencyContactInfo extends StatefulWidget {
 
 class _EmergencyContactInfoState extends State<EmergencyContactInfo> {
   final NewAccount account;
+  final _formKey = GlobalKey<FormState>();
+  String lastName,
+      firstName,
+      middleInitial,
+      gender,
+      birthDate,
+      address,
+      contactNo,
+      relationship;
 
   _EmergencyContactInfoState(this.account);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 25.0),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "Emergency Contact Information",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.bold),
+          padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 25.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Emergency Contact Information",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              child: Row(children: <Widget>[
-                Text(
-                  "Last Name",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
                 ),
-              ]),
-            ),
-            SizedBox(height: 5),
-            TextInput(obscureText: false),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text(
-                  "First Name",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
+                SizedBox(height: 20),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text(
+                      "Last Name",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
                 ),
-              ]),
-            ),
-            SizedBox(height: 5),
-            TextInput(obscureText: false),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text(
-                  "Middle Name",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.normal),
+                SizedBox(height: 5),
+                TextFormField(
+                    obscureText: false,
+                    onChanged: (val) => setState(() => lastName = val),
+                    validator: (val) =>
+                        val.isEmpty ? 'This field is required' : null),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text(
+                      "First Name",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
                 ),
-              ]),
-            ),
-            SizedBox(height: 5),
-            TextInput(obscureText: false),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text("Address",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.normal)),
-              ]),
-            ),
-            SizedBox(height: 5),
-            TextInput(obscureText: false),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text("Contact Number",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.normal)),
-              ]),
-            ),
-            SizedBox(height: 5),
-            TextInput(obscureText: false),
-            SizedBox(height: 15),
-            Container(
-              child: Row(children: <Widget>[
-                Text("Relationship",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.normal)),
-              ]),
-            ),
-            SizedBox(height: 5),
-            TextInput(obscureText: false),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.only(right: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward_sharp, size: 45),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MedicalHistory(account)),
-                      );
-                    },
+                SizedBox(height: 5),
+                TextFormField(
+                    obscureText: false,
+                    onChanged: (val) => setState(() => firstName = val),
+                    validator: (val) =>
+                        val.isEmpty ? 'This field is required' : null),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text(
+                      "Middle Initial",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "ShipporiMincho",
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ]),
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                    obscureText: false,
+                    onChanged: (val) => setState(() => middleInitial = val),
+                    validator: (val) => val.isEmpty
+                        ? 'This field is required'
+                        : val.length > 1
+                            ? 'Please enter 1 character only'
+                            : null),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text("Address",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.normal)),
+                  ]),
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                    obscureText: false,
+                    onChanged: (val) => setState(() => address = val),
+                    validator: (val) =>
+                        val.isEmpty ? 'This field is required' : null),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text("Contact Number",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.normal)),
+                  ]),
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                    obscureText: false,
+                    onChanged: (val) => setState(() => contactNo = val),
+                    validator: (val) =>
+                        val.isEmpty ? 'This field is required' : null),
+                SizedBox(height: 15),
+                Container(
+                  child: Row(children: <Widget>[
+                    Text("Relationship",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "ShipporiMincho",
+                            fontWeight: FontWeight.normal)),
+                  ]),
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                    obscureText: false,
+                    onChanged: (val) => setState(() => relationship = val),
+                    validator: (val) =>
+                        val.isEmpty ? 'This field is required' : null),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.arrow_forward_sharp, size: 45),
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            print(firstName);
+                            print(lastName);
+                            print(middleInitial);
+                            print(address);
+                            print(contactNo);
+                            print(relationship);
+                          }
+
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //       builder: (context) => MedicalHistory(account)),
+                          // );
+                        },
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
