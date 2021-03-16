82e6874e433c732b33b08ce17244e93a8e057985
Veto Bastiero <vetobastiero@gmail.com>
2021-03-16 15:51:58 +0800

    feat: prints user id to console on render

--- lib/components/pages/patient_screen/patient_home_page.dart
+++ lib/components/pages/patient_screen/patient_home_page.dart
@@ -3,6 +3,8 @@ import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/pages/patient_screen/emergency_page.dart';
 import 'package:wellness24/components/pages/doctor_screen/doctor_info_page.dart';
 import 'package:wellness24/components/pages/patient_screen/patient_schedule_page.dart';
+import 'package:provider/provider.dart';
+import 'package:wellness24/models/user.dart';
 
 import '../doctor_search_page/doctor_search_page.dart';
 
@@ -17,6 +19,8 @@ class _PatientHomePageState extends State<PatientHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user.uid);
     return Scaffold(
       appBar: CustomAppBar(
         title: 'Home Page',
