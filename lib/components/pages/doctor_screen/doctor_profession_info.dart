3c5bc7667c52b5b782733e008a1190340fcee7a1
Jedro1270 <jedro1270@gmail.com>
2021-03-17 15:41:10 +0800

    feat: add loading animations after clicking submit

--- lib/components/pages/doctor_screen/doctor_profession_info.dart
+++ lib/components/pages/doctor_screen/doctor_profession_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wellness24/components/common/loading_animation.dart';
 import 'package:wellness24/components/pages/doctor_screen/doctor_home_page.dart';
 import 'package:wellness24/models/new_account.dart';
 import 'package:wellness24/models/user.dart';
@@ -18,217 +19,236 @@ class _DoctorProfessionInfoState extends State<DoctorProfessionInfo> {
   String specialization = 'General Medicine';
   NewAccount account;
   String licenseNo, clinicLoc, clinicStart, clinicEnd;
+  bool loading = false;
 
   _DoctorProfessionInfoState(this.account);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 25.0),
-        child: Form(
-            key: _formKey,
-            child: ListView(
-              children: <Widget>[
-                Text(
-                  "Doctor Information Form",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: "ShipporiMincho",
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          "Specialization",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: "ShipporiMincho",
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ]),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 50,
-                        child: DropdownButton(
-                          value: specialization,
-                          items: [
-                            DropdownMenuItem(
-                              child: Text("General Medicine",
-                                  style:
-                                      TextStyle(fontFamily: "ShipporiMincho")),
-                              value: "General Medicine",
-                            ),
-                            DropdownMenuItem(
-                              child: Text("Neurologist",
-                                  style:
-                                      TextStyle(fontFamily: "ShipporiMincho")),
-                              value: "Neurologist",
-                            ),
-                            DropdownMenuItem(
-                              child: Text("Psychiatrist",
-                                  style:
-                                      TextStyle(fontFamily: "ShipporiMincho")),
-                              value: "Psychiatrist",
-                            ),
-                            DropdownMenuItem(
-                              child: Text("Pediatrician",
-                                  style:
-                                      TextStyle(fontFamily: "ShipporiMincho")),
-                              value: "Pediatrician",
-                            )
-                          ],
-                          onChanged: (val) =>
-                              setState(() => specialization = val),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  child: Row(children: <Widget>[
-                    Text(
-                      "License No.",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: "ShipporiMincho",
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ]),
-                ),
-                SizedBox(
-                    height: 50,
-                    child: TextFormField(
-                        obscureText: false,
-                        onChanged: (val) => setState(() => licenseNo = val),
-                        validator: (val) =>
-                            val.isEmpty ? 'This field is required' : null)),
-                SizedBox(height: 20),
-                Container(
-                  child: Row(
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.white,
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 25.0),
+              child: Form(
+                  key: _formKey,
+                  child: ListView(
                     children: <Widget>[
                       Text(
-                        "Clinic Location",
+                        "Doctor Information Form",
                         style: TextStyle(
+                            color: Colors.black,
                             fontSize: 20,
                             fontFamily: "ShipporiMincho",
-                            fontWeight: FontWeight.normal),
+                            fontWeight: FontWeight.bold),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: TextFormField(
-                      obscureText: false,
-                      onChanged: (val) => setState(() => clinicLoc = val),
-                      validator: (val) =>
-                          val.isEmpty ? 'This field is required' : null),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  child: Row(children: <Widget>[
-                    Text(
-                      "Clinic Hours.",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: "ShipporiMincho",
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ]),
-                ),
-                Container(
-                  child: Row(
-                    children: <Widget>[
-                      SizedBox(
-                        width: 150,
-                        child: TextFormField(
-                            obscureText: false,
-                            onChanged: (val) =>
-                                setState(() => clinicStart = val),
-                            validator: (val) =>
-                                val.isEmpty ? 'This field is required' : null),
+                      SizedBox(height: 20),
+                      Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Specialization",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontFamily: "ShipporiMincho",
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ]),
                       ),
-                      SizedBox(width: 10),
-                      Text(
-                        "to",
-                        style: TextStyle(
-                            fontSize: 20, fontFamily: "ShipporiMincho"),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              height: 50,
+                              child: DropdownButton(
+                                value: specialization,
+                                items: [
+                                  DropdownMenuItem(
+                                    child: Text("General Medicine",
+                                        style: TextStyle(
+                                            fontFamily: "ShipporiMincho")),
+                                    value: "General Medicine",
+                                  ),
+                                  DropdownMenuItem(
+                                    child: Text("Neurologist",
+                                        style: TextStyle(
+                                            fontFamily: "ShipporiMincho")),
+                                    value: "Neurologist",
+                                  ),
+                                  DropdownMenuItem(
+                                    child: Text("Psychiatrist",
+                                        style: TextStyle(
+                                            fontFamily: "ShipporiMincho")),
+                                    value: "Psychiatrist",
+                                  ),
+                                  DropdownMenuItem(
+                                    child: Text("Pediatrician",
+                                        style: TextStyle(
+                                            fontFamily: "ShipporiMincho")),
+                                    value: "Pediatrician",
+                                  )
+                                ],
+                                onChanged: (val) =>
+                                    setState(() => specialization = val),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      SizedBox(width: 10),
-                      SizedBox(
-                        width: 150,
-                        child: TextFormField(
-                            obscureText: false,
-                            onChanged: (val) => setState(() => clinicEnd = val),
-                            validator: (val) =>
-                                val.isEmpty ? 'This field is required' : null),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 50.0),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        width: 150.0,
-                        child: MaterialButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                              side: BorderSide(color: Colors.black12)),
-                          color: Colors.grey.withOpacity(0.5),
-                          minWidth: MediaQuery.of(context).size.width,
-                          padding: EdgeInsets.fromLTRB(18.0, 15.0, 18.0, 15.0),
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              User doctor = await account.registerDoctor(
-                                  licenseNo: licenseNo,
-                                  clinicLocation: clinicLoc,
-                                  clinicStart: clinicStart,
-                                  clinicEnd: clinicEnd,
-                                  specialization: specialization);
-
-                              final database = DatabaseService(uid: doctor.uid);
-
-                              database.insertDoctor(account);
-
-                              if (doctor != null) {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            DoctorHomePage()));
-                              }
-                            }
-                          },
-                          child: Text(
-                            "Login",
+                      SizedBox(height: 20),
+                      Container(
+                        child: Row(children: <Widget>[
+                          Text(
+                            "License No.",
                             style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 25,
+                                fontSize: 20,
                                 fontFamily: "ShipporiMincho",
                                 fontWeight: FontWeight.normal),
                           ),
+                        ]),
+                      ),
+                      SizedBox(
+                          height: 50,
+                          child: TextFormField(
+                              obscureText: false,
+                              onChanged: (val) =>
+                                  setState(() => licenseNo = val),
+                              validator: (val) => val.isEmpty
+                                  ? 'This field is required'
+                                  : null)),
+                      SizedBox(height: 20),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              "Clinic Location",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: "ShipporiMincho",
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: TextFormField(
+                            obscureText: false,
+                            onChanged: (val) => setState(() => clinicLoc = val),
+                            validator: (val) =>
+                                val.isEmpty ? 'This field is required' : null),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        child: Row(children: <Widget>[
+                          Text(
+                            "Clinic Hours.",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: "ShipporiMincho",
+                                fontWeight: FontWeight.normal),
+                          ),
+                        ]),
+                      ),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width: 150,
+                              child: TextFormField(
+                                  obscureText: false,
+                                  onChanged: (val) =>
+                                      setState(() => clinicStart = val),
+                                  validator: (val) => val.isEmpty
+                                      ? 'This field is required'
+                                      : null),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "to",
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "ShipporiMincho"),
+                            ),
+                            SizedBox(width: 10),
+                            SizedBox(
+                              width: 150,
+                              child: TextFormField(
+                                  obscureText: false,
+                                  onChanged: (val) =>
+                                      setState(() => clinicEnd = val),
+                                  validator: (val) => val.isEmpty
+                                      ? 'This field is required'
+                                      : null),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 50.0),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              width: 150.0,
+                              child: MaterialButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                    side: BorderSide(color: Colors.black12)),
+                                color: Colors.grey.withOpacity(0.5),
+                                minWidth: MediaQuery.of(context).size.width,
+                                padding:
+                                    EdgeInsets.fromLTRB(18.0, 15.0, 18.0, 15.0),
+                                onPressed: () async {
+                                  if (_formKey.currentState.validate()) {
+                                    setState(() {
+                                      loading = true;
+                                    });
+
+                                    User doctor = await account.registerDoctor(
+                                        licenseNo: licenseNo,
+                                        clinicLocation: clinicLoc,
+                                        clinicStart: clinicStart,
+                                        clinicEnd: clinicEnd,
+                                        specialization: specialization);
+
+                                    final database =
+                                        DatabaseService(uid: doctor.uid);
+
+                                    database.insertDoctor(account);
+
+                                    if (doctor == null) {
+                                      setState(() {
+                                        // TO DO: add error message
+                                        loading = false;
+                                      });
+                                    } else {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  DoctorHomePage()));
+                                    }
+                                  }
+                                },
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 25,
+                                      fontFamily: "ShipporiMincho",
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
-                  ),
-                ),
-              ],
-            )),
-      ),
-    );
+                  )),
+            ),
+          );
   }
 }
