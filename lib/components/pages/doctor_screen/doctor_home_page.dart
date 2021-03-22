20235926e503e13066aa0e765874b4677b14d8b9
Casey Rhyss Capacio <capaciocasey@gmail.com>
2021-03-22 17:01:54 +0800

    feat: added appointment list

--- lib/components/pages/doctor_screen/doctor_home_page.dart
+++ lib/components/pages/doctor_screen/doctor_home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/common/navigation_bar.dart';
 import 'package:provider/provider.dart';
+import 'package:wellness24/components/common/schedule_card.dart';
 import 'package:wellness24/components/pages/doctor_screen/notification_page.dart';
 import 'package:wellness24/components/pages/doctor_screen/patients_list/patients_list.dart';
 import 'package:wellness24/models/user.dart';
@@ -78,34 +79,47 @@ class _DoctorHomePageState extends State<DoctorHomePage> {
                 ],
               ),
             ),
-            SizedBox(height: 10.0),
-            Container(
-              padding: EdgeInsets.only(left: 40.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 60,
-                    width: 60,
-                    child: Image(image: AssetImage("assets/schedule.png")),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Text(
-                      "My Appointments",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontFamily: "ShipporiMincho",
-                          fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
+            SizedBox(height: 30.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'My Appointments',
+                style: TextStyle(
+                  fontFamily: "ShipporiMincho",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
               ),
             ),
+            SizedBox(height: 30.0),
+            buildAppointmentList(),
+            SizedBox(height: 30.0),
           ],
         ),
       ),
     );
   }
+}
+
+buildAppointmentList() {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 30),
+    child: Column(
+      children: <Widget>[
+        ScheduleCard(
+          '12',
+          'Jan',
+          'Consultation',
+          'Sunday 9am - 11am'
+        ),
+        SizedBox(height: 20),
+        ScheduleCard(
+          '15',
+          'Oct',
+          'Consultation',
+          'Monday 1pm - 3pm'
+        )
+      ],
+    ),
+  );
 }
