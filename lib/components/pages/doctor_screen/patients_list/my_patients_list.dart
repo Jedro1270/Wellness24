104d8581f9e5f94ce3d865f31e621fb50a2b3438
Jedro1270 <jedro1270@gmail.com>
2021-03-23 20:39:52 +0800

    chore: rename to my_patients_list

--- lib/components/pages/doctor_screen/patients_list/my_patients_list.dart
+++ lib/components/pages/doctor_screen/patients_list/my_patients_list.dart
@@ -1,21 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/pages/doctor_screen/patients_list/patient_info.dart';
 import 'package:wellness24/models/patient.dart';
 import 'package:wellness24/services/database.dart';
 
-class PatientsList extends StatefulWidget {
+class MyPatientsList extends StatefulWidget {
   final CollectionReference doctorDatabaseRef;
   final String doctorId;
 
-  PatientsList({this.doctorDatabaseRef, this.doctorId});
+  MyPatientsList({this.doctorDatabaseRef, this.doctorId});
 
   @override
-  _PatientsListState createState() => _PatientsListState();
+  _MyPatientsListState createState() => _MyPatientsListState();
 }
 
-class _PatientsListState extends State<PatientsList> {
+class _MyPatientsListState extends State<MyPatientsList> {
   List<PatientInfo> patients = [];
   DatabaseService database;
 
