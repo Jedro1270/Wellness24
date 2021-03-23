ce9105315624559dab0410ed4f1eb24c4bc46ece
elim23 <elimjoyce.abagat-19@cpu.edu.ph>
2021-03-23 16:35:00 +0800

    feat: change icon to back button in Notif page

--- lib/components/pages/doctor_screen/notification_page.dart
+++ lib/components/pages/doctor_screen/notification_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/common/navigation_bar.dart';
 import 'package:wellness24/components/common/view_request_component.dart';
+import 'package:wellness24/components/pages/doctor_screen/doctor_home_page.dart';
 
 class NotificationPage extends StatefulWidget {
   @override
@@ -12,18 +13,23 @@ class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavBar(),
-      appBar: CustomAppBar(
-        title: 'Notifications',
-        actions: [
-          IconButton(
-              icon: Icon(Icons.search),
+        drawer: NavBar(),
+        appBar: CustomAppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
               onPressed: () {
-                print("Clicked Search icon");
-              })
-        ],
-      ),
-      body: ViewRequest()
-    );
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DoctorHomePage()));
+              }),
+          title: 'Notifications',
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  print("Clicked Search icon");
+                })
+          ],
+        ),
+        body: ViewRequest());
   }
 }
