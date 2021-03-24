9e24397c79cfa5d72514f16288a953985119f23b
Jedro1270 <jedro1270@gmail.com>
2021-03-24 09:39:22 +0800

    feat: add additional patient conditions

--- lib/components/pages/common_pages/patient_profile/patient_profile.dart
+++ lib/components/pages/common_pages/patient_profile/patient_profile.dart
@@ -5,6 +5,7 @@ import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/common/loading_animation.dart';
 import 'package:wellness24/components/pages/common_pages/patient_profile/patient_condition.dart';
 import 'package:wellness24/models/blood_pressure.dart';
+import 'package:wellness24/models/blood_sugar_level.dart';
 import 'package:wellness24/models/patient.dart';
 import 'package:wellness24/models/user.dart';
 import 'package:wellness24/services/database.dart';
@@ -20,9 +21,12 @@ class PatientProfile extends StatefulWidget {
 }
 
 class _PatientProfileState extends State<PatientProfile> {
-  TextEditingController controller;
-  bool editing = false;
+  TextEditingController bloodPressureController;
+  TextEditingController bloodSugarController;
+  bool editingBloodPressure = false;
+  bool edititngBloodSugarLevel = false;
   BloodPressure newBloodPressure;
+  BloodSugarLevel newBloodSugarLevel;
 
   @override
   void didChangeDependencies() {
@@ -35,10 +39,16 @@ class _PatientProfileState extends State<PatientProfile> {
 
     if (widget.patient != null) {
       newBloodPressure = widget.patient.bloodPressure;
+      newBloodSugarLevel = widget.patient.bloodSugarLevel;
 
       if (widget.patient.bloodPressure != null) {
-        controller =
+        bloodPressureController =
             TextEditingController(text: widget.patient.bloodPressure.reading);
+      }
+
+      if (widget.patient.bloodSugarLevel != null) {
+        bloodSugarController =
+            TextEditingController(text: widget.patient.bloodSugarLevel.reading);
       }
     }
   }
@@ -71,7 +81,8 @@ class _PatientProfileState extends State<PatientProfile> {
                       children: <Widget>[
                         ClipOval(
                           child: Image(
-                            image: AssetImage('assets/sample-patient.jpg'), // _imageUrl
+                            image: AssetImage(
+                                'assets/sample-patient.jpg'), // _imageUrl
                             width: 180,
                             height: 180,
                             fit: BoxFit.cover,
@@ -117,10 +128,30 @@ class _PatientProfileState extends State<PatientProfile> {
                   PatientCondition(
                     editable: widget.editable,
                     icon: Image(image: AssetImage('assets/weight.png')),
+                    content: widget.patient.bodyTemperature.toString(),
+                    title: 'Body Temperature',
+                    onChanged: (newContent) async {
+                      widget.patient.bodyTemperature = double.parse(newContent);
+                      await database.updatePatient(widget.patient);
+                    },
+                  ),
+                  PatientCondition(
+                    editable: widget.editable,
+                    icon: Image(image: AssetImage('assets/weight.png')),
                     content: widget.patient.weight.toString(),
                     title: 'Weight',
                     onChanged: (newContent) async {
                       widget.patient.weight = double.parse(newContent);
+                      await database.updatePatient(widget.patient);
+                    },
+                  ),
+                  PatientCondition(
+                    editable: widget.editable,
+                    icon: Image(image: AssetImage('assets/weight.png')),
+                    content: widget.patient.height.toString(),
+                    title: 'Height',
+                    onChanged: (newContent) async {
+                      widget.patient.height = double.parse(newContent);
                       await database.updatePatient(widget.patient);
                     },
                   ),
@@ -145,7 +176,8 @@ class _PatientProfileState extends State<PatientProfile> {
                         Icons.cake_outlined,
                         size: 30,
                       ),
-                      content: DateFormat.yMd().format(widget.patient.birthDate),
+                      content:
+                          DateFormat.yMd().format(widget.patient.birthDate),
                       title: 'Birthday',
                       onChanged: (newContent) async {
                         widget.patient.birthDate = newContent;
@@ -156,7 +188,7 @@ class _PatientProfileState extends State<PatientProfile> {
                   InkWell(
                     onTap: () {
                       setState(() {
-                        editing = true;
+                        editingBloodPressure = true;
                       });
                     },
                     child: Container(
@@ -168,7 +200,7 @@ class _PatientProfileState extends State<PatientProfile> {
                                   fontSize: 20,
                                   fontFamily: 'ShipporiMincho')),
                           SizedBox(width: 20),
-                          editing && widget.editable
+                          editingBloodPressure && widget.editable
                               ? SizedBox(
                                   height: 50,
                                   width: 100,
@@ -178,14 +210,14 @@ class _PatientProfileState extends State<PatientProfile> {
                                         newBloodPressure = BloodPressure(
                                             reading: newValue,
                                             lastChecked: DateTime.now());
-                                        editing = false;
+                                        editingBloodPressure = false;
                                         widget.patient.bloodPressure =
                                             newBloodPressure;
                                         database.updatePatient(widget.patient);
                                       });
                                     },
                                     autofocus: true,
-                                    controller: controller,
+                                    controller: bloodPressureController,
                                   ),
                                 )
                               : Text(
@@ -211,7 +243,8 @@ class _PatientProfileState extends State<PatientProfile> {
                                           : widget.patient.bloodPressure
                                               .sinceLastChecked,
                                       style: TextStyle(
-                                          fontFamily: 'ShipporiMincho'),
+                                          fontFamily: 'ShipporiMincho',
+                                          color: Colors.white),
                                     ),
                                   ],
                                 )),
@@ -220,7 +253,75 @@ class _PatientProfileState extends State<PatientProfile> {
                       ),
                     ),
                   ),
-                  Divider(height: 40,color: Colors.black),
+                  Divider(height: 40, color: Colors.black),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        edititngBloodSugarLevel = true;
+                      });
+                    },
+                    child: Container(
+                      child: Row(
+                        children: <Widget>[
+                          Text('Blood Sugar Level:',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  fontFamily: 'ShipporiMincho')),
+                          SizedBox(width: 20),
+                          edititngBloodSugarLevel && widget.editable
+                              ? SizedBox(
+                                  height: 50,
+                                  width: 100,
+                                  child: TextField(
+                                    onSubmitted: (newValue) {
+                                      setState(() {
+                                        newBloodSugarLevel = BloodSugarLevel(
+                                            reading: newValue,
+                                            lastChecked: DateTime.now());
+                                        edititngBloodSugarLevel = false;
+                                        widget.patient.bloodSugarLevel =
+                                            newBloodSugarLevel;
+                                        database.updatePatient(widget.patient);
+                                      });
+                                    },
+                                    autofocus: true,
+                                    controller: bloodSugarController,
+                                  ),
+                                )
+                              : Text(
+                                  newBloodSugarLevel == null
+                                      ? ''
+                                      : newBloodSugarLevel.reading,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 15,
+                                      fontFamily: 'ShipporiMincho'),
+                                ),
+                          SizedBox(
+                            height: 20,
+                            width: 80,
+                            child: Container(
+                                color: Colors.redAccent[700],
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      widget.patient.bloodSugarLevel == null
+                                          ? 'NA'
+                                          : widget.patient.bloodSugarLevel
+                                              .sinceLastChecked,
+                                      style: TextStyle(
+                                          fontFamily: 'ShipporiMincho',
+                                          color: Colors.white),
+                                    ),
+                                  ],
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
