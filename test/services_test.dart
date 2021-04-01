b6b39eda3d0af9c2b704caa6873333eaba34cdab
Jedro1270 <jedro1270@gmail.com>
2021-04-01 14:52:19 +0800

    test: finish tests for AuthService

--- test/services_test.dart
+++ test/services_test.dart
@@ -1,20 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wellness24/models/user.dart';
 import 'package:wellness24/services/auth_service.dart';
+
+class FirebaseAuthMock extends Mock implements FirebaseAuth {}
 
 void main() {
   group('AuthService', () {
+    final FirebaseAuthMock firebaseAuthMock = FirebaseAuthMock();
+    final AuthService testAuth = AuthService(auth: firebaseAuthMock);
     group('.signInWithEmailAndPassword ', () {
+      when(firebaseAuthMock.signInWithEmailAndPassword(
+              email: 'test@gmail.com', password: '123'))
+          .thenAnswer((_) => Future<MockAuthResult>.value(MockAuthResult()));
       test('should return a User as a Future if credentials are valid',
-          () async {});
+          () async {
+        final result =
+            await testAuth.signInWithEmailAndPassword('test@gmail.com', '123');
+
+        expect(result, isInstanceOf<User>());
+      });
       test('should return a null as a Future if credentials are invalid',
-          () async {});
+          () async {
+        final wrongPassword = await testAuth.signInWithEmailAndPassword(
+            'test@gmail.com', 'invalidPassword');
+        final wrongEmail = await testAuth.signInWithEmailAndPassword(
+            'wrongEmail@gmail.com', '123');
+
+        expect(wrongPassword, isInstanceOf<Null>());
+        expect(wrongEmail, isInstanceOf<Null>());
+      });
     });
-    group('registerWithEmailAndPassword', () {
+    group('.registerWithEmailAndPassword ', () {
+      when(firebaseAuthMock.createUserWithEmailAndPassword(
+              email: 'newEmail@gmail.com', password: 'newPassword'))
+          .thenAnswer((_) => Future<MockAuthResult>.value(MockAuthResult()));
+
+      when(firebaseAuthMock.createUserWithEmailAndPassword(
+              email: 'takenEmail@gmail.com', password: 'newPassword'))
+          .thenAnswer((_) => Future<MockAuthResult>.value(null));
+
       test('should return a User as a Future if credentials are valid',
-          () async {});
-      test('should return a null as a Future if credentials are invalid',
-          () async {});
+          () async {
+        final result = await testAuth.registerWithEmailAndPassword(
+            'newEmail@gmail.com', 'newPassword');
+
+        expect(result, isInstanceOf<User>());
+      });
+      test('should return a null as a Future if email is already taken',
+          () async {
+        final result = await testAuth.registerWithEmailAndPassword(
+            'takenEmail@gmail.com', 'newPassword');
+
+        expect(result, isInstanceOf<Null>());
+      });
     });
   });
   group('DatabaseService', () {});
