d5fffef15d0d5d513698f4129ee7b05afed310c3
Veto Bastiero <vetobastiero@gmail.com>
2021-04-01 15:14:35 +0800

    test: divides test case on getrole into two

--- test/services_test.dart
+++ test/services_test.dart
@@ -5,27 +5,27 @@ import 'package:wellness24/services/auth_service.dart';
 import 'package:wellness24/services/database.dart';
 
 void main() {
-  group('AuthService', () {
-    group('.signInWithEmailAndPassword ', () {
-      test('should return a User as a Future if credentials are valid',
-          () async {});
-      test('should return a null as a Future if credentials are invalid',
-          () async {});
-    });
-    group('registerWithEmailAndPassword', () {
-      test('should return a User as a Future if credentials are valid',
-          () async {});
-      test('should return a null as a Future if credentials are invalid',
-          () async {});
-    });
-  });
+  // group('AuthService', () {
+  //   group('.signInWithEmailAndPassword ', () {
+  //     test('should return a User as a Future if credentials are valid',
+  //         () async {});
+  //     test('should return a null as a Future if credentials are invalid',
+  //         () async {});
+  //   });
+  //   group('registerWithEmailAndPassword', () {
+  //     test('should return a User as a Future if credentials are valid',
+  //         () async {});
+  //     test('should return a null as a Future if credentials are invalid',
+  //         () async {});
+  //   });
+  // });
   group('DatabaseService', () {
     group('.getRole', () {
       MockFirestoreInstance instance = MockFirestoreInstance();
       String uid = '123';
       DatabaseService database = DatabaseService(uid: uid, firestore: instance);
 
-      test('should return role string either "doctor" or "patient"', () async {
+      test('should return string role "Patient" on patient account"', () async {
         await instance
             .collection('roles')
             .document(uid)
@@ -33,7 +33,9 @@ void main() {
 
         String role = await database.getRole();
         expect(role, 'Patient');
+      });
 
+      test('should return string role "Doctor" on doctor account', () async {
         await instance
             .collection('roles')
             .document(uid)
