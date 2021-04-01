688f7dfc5a89b4a8b825d5e6e21f63a30cbd1930
Veto Bastiero <vetobastiero@gmail.com>
2021-04-01 16:41:25 +0800

    test: adds test case for .insertPatient

--- test/services_test.dart
+++ test/services_test.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:wellness24/models/emergency_contact.dart';
 import 'package:wellness24/models/new_account.dart';
 import 'package:wellness24/services/auth_service.dart';
 import 'package:wellness24/services/database.dart';
@@ -114,7 +115,7 @@ void main() {
 
         // TODO: Fix test case
         // expect(document.data['birthDate'], testAcc.birthDate);
-        // 
+        //
         expect(document.data['middleInitial'], testAcc.middleInitial);
         expect(document.data['address'], testAcc.address);
         expect(document.data['gender'], testAcc.gender);
@@ -126,6 +127,55 @@ void main() {
         expect(document.data['specialization'], testAcc.specialization);
         expect(document.data['education'], testAcc.education);
         expect(document.data['about'], testAcc.about);
+      });
+    });
+
+    group('.insertPatient', () {
+      test('should insert newacc object to firestore as patient', () async {
+        EmergencyContact testContact = EmergencyContact(
+            lastName: 'Bastiero',
+            firstName: 'Mom',
+            middleInitial: 'F',
+            address: 'Cabatuan, Iloilo',
+            contactNo: '09221231232',
+            relationship: 'Mother');
+
+        NewAccount testAcc = NewAccount('Patient');
+        testAcc.keywords = ['v', 've', 'vet', 'veto'];
+        testAcc.email = 'veto@gmail.com';
+        testAcc.contactNo = '09123312312';
+        testAcc.lastName = 'Bastiero';
+        testAcc.firstName = 'Veto';
+        testAcc.middleInitial = 'Y';
+        testAcc.birthDate = DateTime(2000, 1, 1);
+        testAcc.address = 'Cabatuan, Iloilo';
+        testAcc.gender = 'Male';
+        testAcc.medicalHistory = ['Anemia', 'Diabetes', 'AIDS'];
+        testAcc.emergencyContact = testContact;
+
+        MockFirestoreInstance instance = MockFirestoreInstance();
+        String uid = '123';
+        DatabaseService database =
+            DatabaseService(uid: uid, firestore: instance);
+        await database.insertPatient(testAcc);
+        DocumentSnapshot patientDoc =
+            await instance.collection('patients').document(uid).get();
+
+        expect(patientDoc.data['keywords'], testAcc.keywords);
+        expect(patientDoc.data['email'], testAcc.email);
+        expect(patientDoc.data['contactNumber'], testAcc.contactNo);
+        expect(patientDoc.data['lastName'], testAcc.lastName);
+        expect(patientDoc.data['firstName'], testAcc.firstName);
+
+        // TODO: Fix test case
+        // expect(document.data['birthDate'], testAcc.birthDate);
+
+        expect(patientDoc.data['middleInitial'], testAcc.middleInitial);
+        expect(patientDoc.data['address'], testAcc.address);
+        expect(patientDoc.data['gender'], testAcc.gender);
+        expect(patientDoc.data['medicalHistory'], testAcc.medicalHistory);
+
+        // TODO: add test cases to check contact
       });
     });
   });
