08eadcf8442b8c35c9c0f1c160d5c19a5b787e5f
Alejandro Jonela III <ajonela46@gmail.com>
2021-04-06 11:14:25 +0800

    refactor: remove hard coded messages on chat room and replace with chat bubble component

--- lib/components/pages/common_pages/chat/chat_room.dart
+++ lib/components/pages/common_pages/chat/chat_room.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wellness24/components/common/app_bar.dart';
+import 'package:wellness24/components/pages/common_pages/chat/chat_bubble.dart';
 
 class ChatRoom extends StatefulWidget {
   @override
@@ -16,81 +17,15 @@ class _ChatRoomState extends State<ChatRoom> {
       body: Container(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 10),
             Expanded(
               child: ListView(
                 children: [
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(width: 5.0),
-                        Expanded(
-                          child: Container(
-                            height: 60,
-                            child: Text(
-                              'Good day Ms. Abagat. I\'m happy to inform you that your covid swab test result last March 9 2021 is negative.',
-                            ),
-                            padding: EdgeInsets.fromLTRB(10.0, 5.0, 5.0, 5.0),
-                            decoration: BoxDecoration(
-                                color: Colors.blue[300],
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                        ),
-                        SizedBox(width: 5.0),
-                        CircleAvatar(
-                          radius: 25.0,
-                          backgroundColor: Colors.grey,
-                          child: ClipOval(
-                            child: SizedBox(
-                                width: 100.0,
-                                height: 100.0,
-                                child: Image(
-                                    image: AssetImage(
-                                        'assets/doctor_sample.png'))),
-                          ),
-                        ),
-                        SizedBox(width: 5.0),
-                      ],
-                    ),
+                  ChatBubble(
+                    isMe: true,
                   ),
-                  SizedBox(height: 10),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(width: 5.0),
-                        CircleAvatar(
-                          radius: 25.0,
-                          backgroundColor: Colors.grey,
-                          child: ClipOval(
-                            child: SizedBox(
-                                width: 100.0,
-                                height: 100.0,
-                                child: Image(
-                                    image: AssetImage(
-                                        'assets/sample-patient.jpg'))),
-                          ),
-                        ),
-                        SizedBox(width: 5.0),
-                        Expanded(
-                          child: Container(
-                            height: 40,
-                            child: Text(
-                              'Hi Doc! Thank you so much. ',
-                            ),
-                            padding: EdgeInsets.fromLTRB(10.0, 10.0, 5.0, 5.0),
-                            decoration: BoxDecoration(
-                                color: Colors.grey[300],
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                        ),
-                        SizedBox(width: 140.0),
-                      ],
-                    ),
-                  ),
+                  ChatBubble(
+                    isMe: false,
+                  )
                 ],
               ),
             ),
