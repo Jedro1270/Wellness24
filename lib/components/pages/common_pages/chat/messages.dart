c2700c8ae0db757fd72a6bceb50653fb44b0fcb8
Jedro1270 <jedro1270@gmail.com>
2021-04-06 14:46:41 +0800

    feat: replace placholders with firestore data

--- lib/components/pages/common_pages/chat/messages.dart
+++ lib/components/pages/common_pages/chat/messages.dart
@@ -1,57 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wellness24/components/common/app_bar.dart';
-import 'package:wellness24/components/pages/common_pages/chat/chat_room.dart';
+import 'package:wellness24/components/pages/common_pages/chat/user_tile.dart';
+import 'package:wellness24/models/doctor.dart';
+import 'package:wellness24/models/patient.dart';
+import 'package:wellness24/models/user.dart';
+import 'package:wellness24/services/database.dart';
 
 class Messages extends StatefulWidget {
+  final User currentUser;
+
+  Messages({this.currentUser});
+
   @override
   _MessagesState createState() => _MessagesState();
 }
 
 class _MessagesState extends State<Messages> {
+  List<UserTile> users = [];
+  DatabaseService database;
+
+  void getUsers() async {
+    database = DatabaseService(uid: widget.currentUser.uid);
+    String role = await database.getRole();
+
+    List<UserTile> mappedUsers = [];
+
+    DocumentSnapshot snapshot = role == 'Patient'
+        ? await database.patients.document(widget.currentUser.uid).get()
+        : await database.doctors.document(widget.currentUser.uid).get();
+
+    if (role == 'Patient') {
+      await Future.forEach(snapshot.data['doctors'], (doctor) async {
+        Doctor currentDoctor = await database.getDoctor(doctor['uid']);
+
+        mappedUsers.add(UserTile(name: currentDoctor.fullName));
+      });
+    } else {
+      await Future.forEach(snapshot.data['patients'], (patient) async {
+        Patient currentPatient = await database.getPatient(patient['uid']);
+
+        mappedUsers.add(UserTile(name: currentPatient.fullName));
+      });
+    }
+
+    setState(() {
+      users = mappedUsers;
+    });
+  }
+
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
         title: 'Messages',
       ),
-      body: Container(
-          child: ListView(children: <Widget>[
-        InkWell(
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => ChatRoom()));
-          },
-          child: Container(
-            margin: EdgeInsets.all(10),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 30.0,
-                  backgroundColor: Colors.grey,
-                  child: ClipOval(
-                    child: SizedBox(
-                        width: 100.0,
-                        height: 100.0,
-                        child: Image(
-                            image: AssetImage('assets/sample-patient.jpg'))),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Elim C. Abagat",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "ShipporiMincho",
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Divider(thickness: 2),
-      ])),
+      body: Container(child: ListView(children: users)),
     );
   }
 }
