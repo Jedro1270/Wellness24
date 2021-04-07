835b446cb56086dda0c39b4d6917820ff11e9214
Casey Rhyss Capacio <capaciocasey@gmail.com>
2021-04-07 18:47:27 +0800

    test: test for emergency contact registration

---
+++ test/patient_emergency_contact_info_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wellness24/components/pages/patient_screen/medical_histories/medical_history.dart';
+import 'package:wellness24/components/pages/patient_screen/patient_emergency_contact_info.dart';
+import 'package:wellness24/models/new_account.dart';
+
+void main() {
+  testWidgets(
+  'Should alert when a textfield is empty',
+  (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      await tester.pumpWidget(MaterialApp(
+        home: EmergencyContactInfo(NewAccount('Patient')),
+      ));
+
+      final arrowBtnFinder = find.byType(IconButton, skipOffstage: false);
+      await tester.tap(arrowBtnFinder);
+      await tester.pump();
+
+      final alertFinder = find.text('This field is required', skipOffstage: false);
+      expect(alertFinder, findsNWidgets(6));
+    });
+  });
+}
