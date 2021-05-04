4aac944f0dff1e6f29e68a6e0c4600390613d0af
Jedro1270 <jedro1270@gmail.com>
2021-05-04 14:37:57 +0800

    feat: only allow doctors to add or edit medical records

--- lib/components/pages/common_pages/medical_records/medical_records.dart
+++ lib/components/pages/common_pages/medical_records/medical_records.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/pages/common_pages/medical_records/add_medical_record.dart';
 import 'package:wellness24/components/pages/common_pages/medical_records/record_card.dart';
 import 'package:wellness24/models/patient.dart';
+import 'package:wellness24/models/user.dart';
+import 'package:wellness24/services/database.dart';
 
 class MedicalRecords extends StatefulWidget {
   final Patient patient;
@@ -18,30 +21,55 @@ class _MedicalRecordsState extends State<MedicalRecords> {
   List<RecordCard> healthHistory = [];
   List<RecordCard> medicalRecords = [];
 
+  bool isDoctor = false;
+
   @override
   void initState() {
     if (widget.patient != null) {
       healthHistory = widget.patient.medicalHistory.map((title) {
         return RecordCard(title: title);
       }).toList();
-
-      medicalRecords = [
-        RecordCard(
-          title: 'Blood Test',
-          date: DateTime.now(),
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => AddMedicalRecord(title: 'Blood Test')));
-          },
-        ) // TODO: replace placeholder
-      ];
     }
 
     super.initState();
   }
 
+  _initializeBody(String uid) async {
+    String role = await DatabaseService(uid: uid).getRole();
+
+    if (role == 'Doctor') {
+      if (mounted) {
+        setState(() {
+          isDoctor = true;
+        });
+      }
+    }
+
+    medicalRecords = [
+      RecordCard(
+        title: 'Blood Test',
+        date: DateTime.now(),
+        onTap: isDoctor
+            ? () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddMedicalRecord(
+                              title: 'Blood Test',
+                              patient: widget.patient,
+                            )));
+              }
+            : null,
+      ) // TODO: replace placeholder
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
+    User currentUser = Provider.of<User>(context);
+
+    _initializeBody(currentUser.uid);
+
     return Scaffold(
       appBar: CustomAppBar(
         title: 'Medical Records',
@@ -53,13 +81,18 @@ class _MedicalRecordsState extends State<MedicalRecords> {
               })
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => AddMedicalRecord()));
-        },
-      ),
+      floatingActionButton: isDoctor
+          ? FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            AddMedicalRecord(patient: widget.patient)));
+              },
+            )
+          : Container(),
       body: Container(
         child: Column(
           children: [
