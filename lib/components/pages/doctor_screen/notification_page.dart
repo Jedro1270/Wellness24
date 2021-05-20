34cc99c862478c95b5f8478395064d6b0c50b400
Veto Bastiero <vetobastiero@gmail.com>
2021-05-20 15:58:13 +0800

    fix: adds null user safety

--- lib/components/pages/doctor_screen/notification_page.dart
+++ lib/components/pages/doctor_screen/notification_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/common/navigation_bar.dart';
+import 'package:wellness24/components/pages/common_pages/login_page.dart';
 import 'package:wellness24/components/pages/doctor_screen/view_request_component.dart';
 import 'package:wellness24/components/pages/doctor_screen/doctor_home_page.dart';
 import 'package:wellness24/models/user.dart';
@@ -16,6 +17,11 @@ class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
+
+    if (user == null) {
+      return Login();
+    }
+
     return Scaffold(
         drawer: NavBar(),
         appBar: CustomAppBar(
