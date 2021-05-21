abd912ab0a49880810c676a4a0a115782ab89ad2
elim23 <elimjoyce.abagat-19@cpu.edu.ph>
2021-05-21 19:46:47 +0800

    fix: view all button in medical records

--- lib/components/pages/common_pages/medical_records/medical_records.dart
+++ lib/components/pages/common_pages/medical_records/medical_records.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:wellness24/components/common/app_bar.dart';
 import 'package:wellness24/components/pages/common_pages/medical_records/medical_record_page.dart';
 import 'package:wellness24/components/pages/common_pages/medical_records/record_card.dart';
+import 'package:wellness24/components/pages/patient_screen/medical_histories/medical_history.dart';
 import 'package:wellness24/models/medical_record.dart';
 import 'package:wellness24/models/patient.dart';
 import 'package:wellness24/models/user.dart';
@@ -122,7 +123,7 @@ class _MedicalRecordsState extends State<MedicalRecords> {
                       ),
                       TextButton(
                           onPressed: () {
-                            modalList(context);
+                            modalListHistory(context);
                           },
                           child: Text(
                             'View all',
@@ -176,7 +177,14 @@ class _MedicalRecordsState extends State<MedicalRecords> {
                           : Container(),
                       TextButton(
                           onPressed: () {
-                            modalList(context);
+                            isDoctor
+                                ? modalListRecords(context)
+                                : showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                        title: Text('Notice'),
+                                        content: Text(
+                                            'Patients are Prohibited to view Medical Records')));
                           },
                           child: Text(
                             'View all',
@@ -191,7 +199,7 @@ class _MedicalRecordsState extends State<MedicalRecords> {
             ])));
   }
 
-  modalList(context) {
+  modalListHistory(context) {
     return showGeneralDialog(
         context: context,
         transitionDuration: Duration(microseconds: 1000),
@@ -226,10 +234,75 @@ class _MedicalRecordsState extends State<MedicalRecords> {
                             fontFamily: "ShipporiMincho",
                             fontWeight: FontWeight.bold),
                       )),
-                      SizedBox(
-                          height: 150,
-                          child: ListView(
-                              primary: false, children: medicalRecordCards)),
+                      Expanded(
+                        flex: 8,
+                        child:
+                            ListView(primary: false, children: healthHistory),
+                      ),
+                    ],
+                  )));
+        });
+  }
+
+  modalListRecords(context) {
+    return showGeneralDialog(
+        context: context,
+        transitionDuration: Duration(microseconds: 1000),
+        pageBuilder: (BuildContext context, Animation first, Animation second) {
+          return Scaffold(
+              body: Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blueAccent[100],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                            alignment: Alignment.topRight,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(Icons.close)),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'Medical Records',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontFamily: "ShipporiMincho",
+                                fontWeight: FontWeight.bold),
+                          ),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MedicalRecordPage(
+                                            patient: widget.patient,
+                                            createNewRecord: true,
+                                            refresh: refresh,
+                                          )));
+                            },
+                            icon: Icon(Icons.add),
+                            label: Text('Add Medical Record'),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.orange[200],
+                                onPrimary: Colors.black),
+                          ),
+                          SizedBox(
+                              height: 150,
+                              child: ListView(
+                                  primary: false, children: medicalRecordCards))
+                        ],
+                      )
                     ],
                   )));
         });
