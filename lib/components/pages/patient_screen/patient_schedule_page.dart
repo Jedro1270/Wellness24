23fd7840a6dd035476719ff10d05fdf099922d60
Jedro1270 <jedro1270@gmail.com>
2021-05-31 18:42:35 +0800

    feat: add loading after scheduling an appointment

--- lib/components/pages/patient_screen/patient_schedule_page.dart
+++ lib/components/pages/patient_screen/patient_schedule_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:wellness24/components/common/app_bar.dart';
+import 'package:wellness24/components/common/loading_animation.dart';
 import 'package:wellness24/components/pages/patient_screen/patient_priority_page.dart';
 import 'package:wellness24/models/doctor.dart';
 import 'package:wellness24/models/patient.dart';
@@ -21,6 +22,7 @@ class _PatientAppointmentState extends State<PatientAppointmentPage> {
   DateFormat format = DateFormat('MM-dd-yyyy');
   bool isScheduled = false;
   bool isAvailable = true; // Change to database data
+  bool loading = false;
 
   Future updateScheduledStatus(DateTime date) async {
     bool scheduledStatus = await DatabaseService(uid: widget.currentPatient.uid)
@@ -65,138 +67,142 @@ class _PatientAppointmentState extends State<PatientAppointmentPage> {
               Navigator.pop(context);
             }),
       ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ClipOval(
-                      child: Image(
-                        image: AssetImage('assets/sample-patient.jpg'),
-                        width: 120,
-                        height: 120,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Divider(height: 30, color: Colors.transparent),
-                    Text('Doctor ${widget.doctor.fullName}',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: "ShipporiMincho",
-                            color: Colors.black)),
-                    Divider(height: 30, color: Colors.transparent),
-                    Text(
-                      "Doctor's Schedule",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: "ShipporiMincho",
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    Divider(height: 5, color: Colors.transparent),
-                    Text(widget.doctor.workingDays,
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: "ShipporiMincho",
-                            color: Colors.black)),
-                    Text(
-                        '${widget.doctor.clinicStart} - ${widget.doctor.clinicEnd}',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: "ShipporiMincho",
-                            color: Colors.black)),
-                    Divider(height: 30, color: Colors.transparent),
-                    Row(
+      body: loading
+          ? Loading()
+          : Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("Pick Date:   ",
+                        ClipOval(
+                          child: Image(
+                            image: AssetImage('assets/sample-patient.jpg'),
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Divider(height: 30, color: Colors.transparent),
+                        Text('Doctor ${widget.doctor.fullName}',
+                            textAlign: TextAlign.center,
                             style: TextStyle(
                                 fontSize: 20,
                                 fontFamily: "ShipporiMincho",
                                 color: Colors.black)),
-                        Container(
-                            alignment: Alignment.centerLeft,
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Color(0xFF40BEEE), width: 1),
-                                borderRadius: BorderRadius.circular(10.0)),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 4),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(format.format(_date),
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontFamily: "ShipporiMincho",
-                                        color: Colors.black)),
-                                IconButton(
-                                  icon: Icon(Icons.calendar_today,
-                                      color: Colors.blue),
-                                  onPressed: () {
-                                    setState(() {
-                                      _selectDate(context);
-                                    });
-                                  },
-                                ),
-                              ],
-                            ))
+                        Divider(height: 30, color: Colors.transparent),
+                        Text(
+                          "Doctor's Schedule",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: "ShipporiMincho",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        Divider(height: 5, color: Colors.transparent),
+                        Text(widget.doctor.workingDays,
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: "ShipporiMincho",
+                                color: Colors.black)),
+                        Text(
+                            '${widget.doctor.clinicStart} - ${widget.doctor.clinicEnd}',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: "ShipporiMincho",
+                                color: Colors.black)),
+                        Divider(height: 30, color: Colors.transparent),
+                        Row(
+                          children: [
+                            Text("Pick Date:   ",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontFamily: "ShipporiMincho",
+                                    color: Colors.black)),
+                            Container(
+                                alignment: Alignment.centerLeft,
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Color(0xFF40BEEE), width: 1),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 4),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(format.format(_date),
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontFamily: "ShipporiMincho",
+                                            color: Colors.black)),
+                                    IconButton(
+                                      icon: Icon(Icons.calendar_today,
+                                          color: Colors.blue),
+                                      onPressed: () {
+                                        setState(() {
+                                          _selectDate(context);
+                                        });
+                                      },
+                                    ),
+                                  ],
+                                ))
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-              Divider(height: 30, color: Colors.transparent),
-              isAvailable
-                  ? MaterialButton(
-                      minWidth: 200,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          side: BorderSide(color: Colors.black12)),
-                      color: Color(0xFF40BEEE),
-                      onPressed: () {
-                        isScheduled
-                            ? Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => PatientPriorityNumber(
-                                          doctor: widget.doctor,
-                                          date: _date,
-                                          currentPatient: widget.currentPatient,
-                                        )))
-                            : _showDialog(context);
-                      },
-                      child: Text(
-                          isScheduled
-                              ? "View Appointment Number"
-                              : "Schedule Appointment",
-                          style: TextStyle(
+                  ),
+                  Divider(height: 30, color: Colors.transparent),
+                  isAvailable
+                      ? MaterialButton(
+                          minWidth: 200,
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: BorderSide(color: Colors.black12)),
+                          color: Color(0xFF40BEEE),
+                          onPressed: () {
+                            isScheduled
+                                ? Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            PatientPriorityNumber(
+                                              doctor: widget.doctor,
+                                              date: _date,
+                                              currentPatient:
+                                                  widget.currentPatient,
+                                            )))
+                                : _showDialog(context);
+                          },
+                          child: Text(
+                              isScheduled
+                                  ? "View Appointment Number"
+                                  : "Schedule Appointment",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 21,
+                                  fontFamily: "ShipporiMincho",
+                                  fontWeight: FontWeight.normal)))
+                      : Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Colors.red,
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Sorry. your doctor is no longer available for appointments. \n\nPlease schedule for a priority number again tomorrow.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
                               color: Colors.white,
-                              fontSize: 21,
-                              fontFamily: "ShipporiMincho",
-                              fontWeight: FontWeight.normal)))
-                  : Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Colors.red,
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'Sorry. your doctor is no longer available for appointments. \n\nPlease schedule for a priority number again tomorrow.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
+                              fontSize: 20,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-            ],
-          )),
+                ],
+              )),
     );
   }
 
@@ -209,15 +215,21 @@ class _PatientAppointmentState extends State<PatientAppointmentPage> {
               actions: [
                 ElevatedButton(
                     onPressed: () async {
+                      Navigator.pop(context);
+
+                      setState(() {
+                        loading = true;
+                      });
+
                       DatabaseService database =
                           DatabaseService(uid: widget.currentPatient.uid);
                       await database.addAppointment(widget.doctor.uid, _date);
 
                       setState(() {
+                        loading = false;
                         isScheduled = true;
                       });
 
-                      Navigator.pop(context);
                       Navigator.push(
                           context,
                           MaterialPageRoute(
