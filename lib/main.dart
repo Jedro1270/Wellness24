24795b21b9fdf53d4afdc60e5858f09ad84373ae
Jedro1270 <jedro1270@gmail.com>
2021-06-04 07:26:55 +0800

    feat: replace catcher with compatible version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// import 'package:catcher/catcher.dart';
+import 'package:catcher/catcher.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:wellness24/components/auth/auth_wrapper.dart';
@@ -7,34 +7,37 @@ import 'package:wellness24/services/auth_service.dart';
 import 'package:wellness24/models/user.dart';
 
 void main() {
-  // CatcherOptions catcherOptions = CatcherOptions(
-  //   DialogReportMode(),
-  //   [
-  //     EmailManualHandler(
-  //       ['wellness24@gmail.com'],
-  //     )
-  //   ],
-  // );
-  // Catcher(
-  //   runAppFunction: () {
-      runApp(MyApp());
-  //   },
-  //   debugConfig: catcherOptions,
-  //   releaseConfig: catcherOptions,
-  // );
+  CatcherOptions catcherOptions = CatcherOptions(
+    DialogReportMode(),
+    [
+      EmailManualHandler(
+        ['jedro1270@gmail.com'],
+        emailHeader: 'Unexpected Error',
+        emailTitle: 'Error Report Wellness24',
+      )
+    ],
+  );
+
+  Catcher(
+    MyApp(),
+    debugConfig: catcherOptions,
+    releaseConfig: catcherOptions,
+    profileConfig: catcherOptions,
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // navigatorKey: Catcher.navigatorKey,
-      title: 'Wellness24',
-      debugShowCheckedModeBanner: false,
-      home: StreamProvider<User>.value(
-          initialData: null,
-          value: AuthService(auth: FirebaseAuth.instance).user,
-          child: AuthWrapper()),
+    return StreamProvider<User>.value(
+      initialData: null,
+      value: AuthService(auth: FirebaseAuth.instance).user,
+      child: MaterialApp(
+        navigatorKey: Catcher.navigatorKey,
+        title: 'Wellness24',
+        debugShowCheckedModeBanner: false,
+        home: AuthWrapper(),
+      ),
     );
   }
 }
