77a6f846584307715f35fa9f96446be64d4cac97
Veto Bastiero <vetobastiero@gmail.com>
2021-06-07 18:48:22 +0800

    fix: inserts patient on register func

--- lib/models/new_account.dart
+++ lib/models/new_account.dart
@@ -150,11 +150,10 @@ class NewAccount {
     User result =
         await auth.registerWithEmailAndPassword(this.email, this.password);
 
-    this.uid = result.uid;
-    await DatabaseService(uid: this.uid).insertRole(this.role);
-
     if (result != null) {
       this.uid = result.uid;
+      await DatabaseService(uid: this.uid).insertRole(this.role);
+      await DatabaseService(uid: this.uid).insertPatient(this);
     }
 
     return result;
